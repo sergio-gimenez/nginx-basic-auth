655311166291f535f0d1400686ca38cf6dd175fd
Sergio Giménez <sergiogimenezanton@gmail.com>
2023-07-28 16:18:05 +0200

    radius client exposed as an api

--- basic-auth-radius/auth-backend/auth-backend.py
+++ basic-auth-radius/auth-backend/auth-backend.py
@@ -1,24 +1,26 @@
-from __future__ import print_function
-from pyrad.client import Client
-from pyrad.dictionary import Dictionary
-import pyrad.packet
+from fastapi import FastAPI, HTTPException
 
-srv = Client(server="localhost", secret=b"testing123",
-             dict=Dictionary("dictionary"))
+app = FastAPI()
 
-# create request
-req = srv.CreateAuthPacket(code=pyrad.packet.AccessRequest,
-                           User_Name="bob", NAS_Identifier="ThatNAS")
-req["User-Password"] = req.PwCrypt("test")
 
-# send request
-reply = srv.SendPacket(req)
+@app.post("/auth")
+async def authenticate(user: str, password: str):
+    from pyrad.client import Client
+    from pyrad.dictionary import Dictionary
+    import pyrad.packet
 
-if reply.code == pyrad.packet.AccessAccept:
-    print("access accepted")
-else:
-    print("access denied")
+    srv = Client(server="localhost", secret=b"testing123",
+                 dict=Dictionary("dictionary"))
 
-print("Attributes returned by server:")
-for i in reply.keys():
-    print("%s: %s" % (i, reply[i]))
+    # create request
+    req = srv.CreateAuthPacket(code=pyrad.packet.AccessRequest,
+                               User_Name=user, NAS_Identifier="ThatNAS")
+    req["User-Password"] = req.PwCrypt(password)
+
+    # send request
+    reply = srv.SendPacket(req)
+
+    if reply.code == pyrad.packet.AccessAccept:
+        return {"message": "access accepted", "attributes": reply.keys()}
+    else:
+        raise HTTPException(status_code=401, detail="access denied")
