ae36c59b94c502e55bd5cbb078c82241d4b7c901
Sergio Giménez <sergiogimenezanton@gmail.com>
2023-07-31 11:16:50 +0200

    Auth Backend now expects to receive a basic auth header

--- basic-auth-radius/auth-backend/auth_backend.py
+++ basic-auth-radius/auth-backend/auth_backend.py
@@ -1,6 +1,7 @@
+import base64
 import os
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Header
 
 app = FastAPI()
 
@@ -12,10 +13,23 @@ if not RADIUS_SECRET or not RADIUS_SERVER:
 
 
 @app.post("/auth")
-async def authenticate(user: str, password: str):
+async def authenticate(authorization: str = Header(None)):
     from pyrad.client import Client
     from pyrad.dictionary import Dictionary
     import pyrad.packet
+
+    if not authorization:
+        raise HTTPException(
+            status_code=401, detail="missing authorization header")
+
+    auth_type, auth_value = authorization.split(" ", 1)
+
+    if auth_type.lower() != "basic":
+        raise HTTPException(
+            status_code=401, detail="unsupported authorization type")
+
+    auth_value = base64.b64decode(auth_value).decode()
+    user, password = auth_value.split(":", 1)
 
     srv = Client(server=RADIUS_SERVER, secret=RADIUS_SECRET.encode(),
                  dict=Dictionary("dictionary"))
